5a4c8d737c2477345c5772548f938ffe0a358a45
6y6a <gubinalexeymain@gmail.com>
2020-01-02 20:55:03 +0300

    add homepage markup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div >
-      Hello world
+      <Homepage/>
     </div>
   );
 }
