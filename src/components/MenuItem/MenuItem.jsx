e27ea980a7998c8f1bfeae64a836ef59658c5cc2
6y6a <gubinalexeymain@gmail.com>
2020-01-02 23:34:20 +0300

    add styling to menu item

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import './MenuItem.scss'
 
 const MenuItem = ({title, imageUrl, size}) => (
-    <div
-        style={{backgroundImage: `url(${imageUrl})`}}
-        className={`menu-item ${size}`}
-    >
+    <div className={`menu-item ${size}`}>
+        <div className="background-image"  style={{backgroundImage: `url(${imageUrl})`}}></div>
         <div className="content">
             <h1 className="title">{title}</h1>
             <span className="subtitle">Shop now</span>
