5a61f5d9f7206ef8173ecf55443836a9dffb64f3
6y6a <gubinalexeymain@gmail.com>
2020-01-03 22:49:55 +0300

    add styling to button with google

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 import './CustomButton.scss'
 
-const CustomButton = ({children, ...props}) => (
-    <button className='custom-button' {...props}>{children}</button>
+const CustomButton = ({children, isGoogleSignIn, ...props}) => (
+    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...props}>
+        {children}
+    </button>
 )
 
 export default CustomButton
