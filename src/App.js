79c070c0005a82bf9d459f0796cba8f167832cf3
6y6a <gubinalexeymain@gmail.com>
2020-01-03 22:51:56 +0300

    add authorization state to app state

--- src/App.js
+++ src/App.js
@@ -4,18 +4,43 @@ import Header from "./components/Header/Header";
 import Homepage from "./pages/Homepage/Homepage";
 import ShopPage from "./pages/Shop/Shop";
 import SignInAndSignUp from "./pages/SignInAndSignUp/SignInAndSignUp";
-import { Route } from 'react-router-dom'
+import {Route} from 'react-router-dom'
+import {auth} from "./firebase/firebase.utils";
 
 
-function App() {
-  return (
-    <div >
-        <Header/>
-        <Route exact path='/' component={Homepage}/>
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUp}/>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+            this.setState({currentUser: user})
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth()
+    }
+
+    render() {
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser}/>
+
+                <Route exact path='/' component={Homepage}/>
+                <Route path='/shop' component={ShopPage}/>
+                <Route path='/signin' component={SignInAndSignUp}/>
+            </div>
+        );
+    }
+
 }
 
 export default App;
