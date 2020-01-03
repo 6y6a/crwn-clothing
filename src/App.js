7d8cf3afbb123a055608d3d6b8f3dc3d762c6bf5
6y6a <gubinalexeymain@gmail.com>
2020-01-03 17:21:33 +0300

    add signin page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Homepage from "./pages/Homepage/Homepage";
 import ShopPage from "./pages/Shop/Shop";
+import SignInAndSignUp from "./pages/SignInAndSignUp/SignInAndSignUp";
 import { Route } from 'react-router-dom'
 
 
@@ -12,6 +13,7 @@ function App() {
         <Header/>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUp}/>
     </div>
   );
 }
---
+++ src/pages/SignInAndSignUp/SignInAndSignUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './SignInAndSignUp.scss'
+
+const SignInAndSignUp = () => (
+    <div className='sign-in-and-sign-up'>
+        Sign in
+    </div>
+)
+
+export default SignInAndSignUp
