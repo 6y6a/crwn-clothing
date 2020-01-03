eae81fcf1f129fe43ee2afb9d7271e98fc0b86c0
6y6a <gubinalexeymain@gmail.com>
2020-01-03 15:30:53 +0300

    add collection-preview styles and render items

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 import './CollectionPreview.scss'
+import CollectionItem from "../CollectionItem/CollectionItem";
 
 const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
         <h1 className='title'>{title}</h1>
         <div className='preview'>
-            {items.map(item => (
-                <div key={item.id}>{item.name}</div>
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(({id, ...itemProps}) => (
+                <CollectionItem key={id} {...itemProps}/>
             ))}
         </div>
     </div>
