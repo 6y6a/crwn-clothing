db6228ab3db150c9bd158a9ee9171cbfb16fd620
6y6a <gubinalexeymain@gmail.com>
2020-01-03 14:20:52 +0300

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Homepage from "./pages/Homepage/Homepage";
+import {Route} from 'react-router-dom'
+
+const HatsPage = () => (
+    <div>
+        <h1>Hats page</h1>
+    </div>
+)
 
 function App() {
   return (
     <div >
-      <Homepage/>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/hats' component={HatsPage}/>
     </div>
   );
 }
