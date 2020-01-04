ca82ad849b420cbdffeea5257c203db3d5fc140c
6y6a <gubinalexeymain@gmail.com>
2020-01-04 13:35:03 +0300

    finish sign in submit

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -4,7 +4,7 @@ import './SignIn.scss'
 
 import FormInput from "../FormInput/FormInput";
 import CustomButton from "../CustomButton/CustomButton";
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {signInWithGoogle, auth} from '../../firebase/firebase.utils'
 
 class SignIn extends Component {
     constructor(props) {
@@ -16,10 +16,19 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault()
 
-        this.setState({email: '', password: ''})
+        const {email, password} = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+
+            this.setState({email: '', password: ''})
+
+        } catch(error) {
+            console.log(error)
+        }
     }
 
     handleChange = event => {
