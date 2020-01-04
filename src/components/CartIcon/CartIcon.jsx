b30940e7fd10d54313b14b3a3dd39037185143af
6y6a <gubinalexeymain@gmail.com>
2020-01-04 22:51:40 +0300

    connect store for toggle menu

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import {ReactComponent as ShopingIcon} from "../../assets/shoping-bag.svg";
+import {connect} from 'react-redux'
+import {toggleCartHidden} from "../../store/Cart/CartActions";
 import './CartIcon.scss'
 
-const CartIcon = () => (
-    <div className='cart-icon'>
+const CartIcon = ({toggleCartHidden}) => (
+    <div className='cart-icon' onClick={toggleCartHidden}>
         <ShopingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count' >0</span>
     </div>
 )
 
-export default CartIcon
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon)
