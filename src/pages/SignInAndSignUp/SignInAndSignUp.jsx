554a1744292987a8d7c153a7c1c7f1607e7f3942
6y6a <gubinalexeymain@gmail.com>
2020-01-07 14:09:13 +0300

    replace scss to styled-component

--- src/pages/SignInAndSignUp/SignInAndSignUp.jsx
+++ src/pages/SignInAndSignUp/SignInAndSignUp.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-import './SignInAndSignUp.scss'
+import {SignInAndSignUpContainer} from "./SignInAndSignUpStyles";
 
 import SignIn from "../../components/SignIn/SignIn";
 import SignUp from "../../components/SignUp/SignUp";
 
 const SignInAndSignUp = () => (
-    <div className='sign-in-and-sign-up'>
+    <SignInAndSignUpContainer>
         <SignIn />
         <SignUp />
-    </div>
+    </SignInAndSignUpContainer>
 )
 
 export default SignInAndSignUp
