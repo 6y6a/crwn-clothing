423658713e1b49e036b01961d2c1d1be3abd857a
6y6a <gubinalexeymain@gmail.com>
2020-01-07 18:10:03 +0300

    replace scss to styled-component

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import CustomButton from "../CustomButton/CustomButton";
+
 import {connect} from 'react-redux'
 import {addItem} from "../../store/Cart/CartActions";
 
-import './CollectionItem.scss'
+import {
+    AddButton,
+    CollectionItemContainer,
+    NameContainer,
+    PriceContainer,
+    CollectionFooter,
+    BackgroundImage
+} from "./CollectionItemStyles";
 
 const CollectionItem = ({item, addItem}) => {
     const {name, imageUrl, price} = item
     return (
-        <div className="collection-item">
-            <div
-                className='image'
-                style={{backgroundImage: `url(${imageUrl})`}}
-            />
-            <div className="collection-footer">
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <CustomButton inverted onClick={() => addItem(item)}>
+        <CollectionItemContainer>
+            <BackgroundImage imageUrl={imageUrl}/>
+            <CollectionFooter>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </CollectionFooter>
+            <AddButton inverted onClick={() => addItem(item)}>
                 Add to cart
-            </CustomButton>
-        </div>
+            </AddButton>
+        </CollectionItemContainer>
     )
 }
 
