427960ba4743d9388f5c25604b81e85391eb96e6
6y6a <gubinalexeymain@gmail.com>
2020-01-07 19:34:24 +0300

    replace scss to styled-component in menu item

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import './MenuItem.scss'
 import {withRouter} from 'react-router-dom'
 
+import {MenuContent, MenuTitle, MenuSubtitle, MenuItemContainer, BackgroundImage} from "./MenuItemStyles";
+
 const MenuItem = ({title, imageUrl, size, history, match, linkUrl}) => (
-    <div
+    <MenuItemContainer
         className={`menu-item ${size}`}
         onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
-        <div className="background-image"  style={{backgroundImage: `url(${imageUrl})`}}></div>
-        <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className="subtitle">Shop now</span>
-        </div>
-    </div>
+        <BackgroundImage imageUrl={imageUrl} className='background-image'/>
+        <MenuContent className='content'>
+            <MenuTitle>{title}</MenuTitle>
+            <MenuSubtitle>Shop now</MenuSubtitle>
+        </MenuContent>
+    </MenuItemContainer>
 )
 
 export default withRouter(MenuItem)
