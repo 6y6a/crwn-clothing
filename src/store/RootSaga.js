0477e59d1fe7a54ad16417927197df4bf6c8ddb1
6y6a <gubinalexeymain@gmail.com>
2020-01-11 19:17:54 +0300

    cleart cart in saga

--- src/store/RootSaga.js
+++ src/store/RootSaga.js
@@ -1,11 +1,12 @@
 import {all, call} from 'redux-saga/effects'
 import {fetchCollectionStart} from "./Shop/ShopSagas";
 import {userSagas} from "./User/UserSagas";
+import {cartSagas} from "./Cart/CartSagas";
 
 export default function* rootSaga() {
     yield all([
         call(fetchCollectionStart),
-        call(userSagas)
-
+        call(userSagas),
+        call(cartSagas)
     ])
 }
